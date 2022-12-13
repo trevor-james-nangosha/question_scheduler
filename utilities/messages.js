bd65a92b7d3dbce9b0b22b534e573506336469a7
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-12-13 15:48:08 +0300

    message template.
    to be used for sending out campaign info to clients.

---
+++ utilities/messages.js
@@ -0,0 +1,13 @@
+let messages = {
+    welcome: {
+        subject: "Welcome to this community.",
+        content: "Thank you for signing up.",
+    },
+    campaign: {
+        subject: "CRAZY DEALS!!!!!!!!",
+        content: "We have good deals off some products you may like."
+    }
+    
+}
+
+module.exports = messages
