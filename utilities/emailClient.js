3fcf8cbbaba2f6c72ee6c3e7538d2e89c316492d
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-12-13 15:46:40 +0300

    replace test account with actual Ethereal account
    instead of generating a test account every time, use a single pair of
    credentials. works better!!!!!!

--- utilities/emailClient.js
+++ utilities/emailClient.js
@@ -1,41 +1,34 @@
 let nodemailer = require("nodemailer")
 
-async function sendEmail(subject, text, htmlMessage=undefined){
+function sendEmail(from, to, subject, text, htmlMessage=undefined){
     // test account to be used for testing
     // find a real email server to use during production
-    let testAccount = nodemailer.createTestAccount()
-        .then(account => {
-            let transporter = nodemailer.createTransport({
-                host: account.smtp.host,
-                port: account.smtp.host,
-                secure: account.smtp.secure,
-                pool: true,
-                auth: {
-                    user: account.user,
-                    pass: account.pass,
-                }, 
-            })
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        pool: true,
+        secure: false,
+        auth: {
+            user: 'earnestine.mann@ethereal.email',
+            pass: 'hETsVcPgHrDhWbxD1u'
+        }
+    });
 
-            // sending mail with the defined transport object
-            let info = transporter.sendMail({
-                from: 'Nangosha nangosha@example.com',
-                to: 'trevornangosha16@gmail.com',
-                subject,
-                text,
-                html: htmlMessage ? htmlMessage : undefined,
-            }).then(info => {
-                console.log(`Preview URL here: ${nodemailer.getTestMessageUrl(info)}`)
-                return process.exit()
-            })
-              .catch(err => {
-                console.error(`Could not send email: ${err}`)
-                return process.exit(1)
-              })
-        })
-        .catch(err => {
-            console.error(`Could not create test account: ${err}`)
-            return process.exit(1)
-        })
+    // sending mail with the defined transport object
+    transporter.sendMail({
+        from,
+        to,
+        subject,
+        text,
+        html: htmlMessage ? htmlMessage : undefined,
+    }).then(info => {
+        console.log(`Preview URL here: ${nodemailer.getTestMessageUrl(info)}`)
+        return process.exit()
+    }).catch(err => {
+        console.error(`Could not send email: ${err}`)
+        return process.exit(1)
+    })
+
 }
 
 module.exports = sendEmail
